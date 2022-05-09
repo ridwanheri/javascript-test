92419617cc3bbece2b6beed9c80b70a1895efd22
ridwanheri <rewsad123@gmail.com>
2022-05-09 15:11:04 +0700

    done js3

--- Js3.js
+++ Js3.js
@@ -13,8 +13,40 @@ const data = [
 
 function result(data) {
     // your code here
-    let uniqueObjArray = [...new Map(data.map((item) => [item['age'], item])).keys()];
-    return uniqueObjArray;
+    let allkeys = {};
+
+    data.forEach((element) => {
+        let keys = Object.keys(element);
+        keys.forEach((el) => {
+            if (!(el in allkeys)) {
+                allkeys[el] = 0;
+            }
+        });
+    });
+
+    allkeysdata = {};
+
+    Object.keys(allkeys).forEach((element) => {
+        allkeysdata[element] = data[0][element];
+    });
+
+    data.forEach((element) => {
+        let keys = Object.keys(element);
+        keys.forEach((el) => {
+            // console.log(element[el])
+            if (element[el] === allkeysdata[el]) {
+                allkeys[el] = allkeys[el] + 1;
+            }
+        });
+    });
+
+    const result = [];
+    Object.keys(allkeys).forEach((element) => {
+        if (allkeys[element] < data.length) {
+            result.push(element);
+        }
+    });
+    return result;
 }
 
 console.log(result(data));
